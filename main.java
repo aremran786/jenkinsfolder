4bf6125690b52ab4a945e62b8b6dfed85ca271b2
aremran786 <125981012+aremran786@users.noreply.github.com>
2023-10-11 11:54:37 +0530

    Create main.java
    initial commit

---
+++ main.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class main{
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("this is first");
+        System.out.println("this is  secound");
+        System.out.println("this is third ");
+        System.out.println("this os four");
+        System.out.println(new Date());
+        System.out.println("somethong");
+    }
+}
